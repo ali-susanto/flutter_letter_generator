e23cf6a7b114f44f44db85b96f470e1a507e2d3e
Ali Susanto <alisusanto.main@gmail.com>
2023-04-18 09:58:58 +0700

    comment ui testing

--- test/ui_testing.dart
+++ test/ui_testing.dart
@@ -1,19 +1,19 @@
-import 'package:e_letter/screen/content/content_viewModel.dart';
-import 'package:e_letter/screen/mobile/home/home_screen_mobile.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
+// import 'package:e_letter/screen/content/content_viewModel.dart';
+// import 'package:e_letter/screen/mobile/home/home_screen_mobile.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:provider/provider.dart';
 
-void main() {
-  var _contentProvider = MultiProvider(
-    providers: [ChangeNotifierProvider(create: (_) => ContentViewModel())],
-    child: const MaterialApp(home: HomeScreenMobile()),
-  );
-  group('test', () {
-    testWidgets('content list', (WidgetTester tester) async {
-      await tester.pumpWidget(_contentProvider);
-      await tester.pump(const Duration(seconds: 2));
-      expect(find.byType(ListView), findsOneWidget);
-    });
-  });
-}
+// void main() {
+//   var _contentProvider = MultiProvider(
+//     providers: [ChangeNotifierProvider(create: (_) => ContentViewModel())],
+//     child: const MaterialApp(home: HomeScreenMobile()),
+//   );
+//   group('test', () {
+//     testWidgets('content list', (WidgetTester tester) async {
+//       await tester.pumpWidget(_contentProvider);
+//       await tester.pump(const Duration(seconds: 2));
+//       expect(find.byType(ListView), findsOneWidget);
+//     });
+//   });
+// }
